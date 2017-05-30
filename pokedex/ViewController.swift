99e0f19bcf5fbb858f41a8f3f54d26f4d3f3080f
Chetan Rajagiri <crajagiri@gmail.com>
2017-05-30 12:14:55 +0530

    Initial Commit

---
+++ pokedex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex
+//
+//  Created by chetan rajagiri on 30/05/17.
+//  Copyright © 2017 chetan rajagiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
